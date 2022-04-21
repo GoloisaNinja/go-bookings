084546ddeade53de35650ce2f11612b134d48e81
Golodhrim <holidayninjastaff@gmail.com>
2022-04-21 01:06:03 -0400

    init commit

---
+++ cmd/web/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoloisaNinja/go-bookings/pkg/config"
+	"github.com/GoloisaNinja/go-bookings/pkg/handlers"
+	"github.com/GoloisaNinja/go-bookings/pkg/render"
+	"github.com/alexedwards/scs/v2"
+	"log"
+	"net/http"
+	"time"
+)
+
+const PORT = ":8000"
+
+// boolean switch that impacts session cookie secure in both scs and middleware
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+	app.IsProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.IsProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache...")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+	repo := handlers.NewRepository(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Printf("Server is up on port %s", PORT)
+
+	srv := &http.Server{
+		Addr:    PORT,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"github.com/GoloisaNinja/go-bookings/pkg/config"
+	"github.com/GoloisaNinja/go-bookings/pkg/models"
+	"github.com/GoloisaNinja/go-bookings/pkg/render"
+	"net/http"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepository(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (e *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	e.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+func (e *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello Again User!"
+
+	remoteIP := e.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(
+		w, "about.page.tmpl", &models.TemplateData{
+			StringMap: stringMap,
+		},
+	)
+}
